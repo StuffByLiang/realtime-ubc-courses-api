51c2f04228bd3b71a5361846e72da13ed1ae99bf
Liang Liu <stuffbyliang@gmail.com>
2020-08-04 22:41:14 -0700

    added optional mongoose connection string

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ http.globalAgent.maxSockets = 50;
 dotenv.config();
 
 // set up mongoose
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI || "", {useNewUrlParser: true, useUnifiedTopology: true})
 .then(
     () => {
         // success
